cf3a17ddaa50ff2ae09aa79dc973e4973df07086
Adinath0 <bhawaniadinath@gmail.com>
2026-04-01 14:05:48 +0530

    experiment2

--- federated_fraud_system/bank_node/config.py
+++
@@ -1,9 +0,0 @@
-CENTRAL_SERVER = "http://127.0.0.1:8000"
-
-BANK_EMAIL = "admin@bankA.com"
-BANK_PASSWORD = "123456"
-
-S3_BUCKET = "federated-fraud-system"
-
-GLOBAL_MODEL_PATH = "global_model.pkl"
-LOCAL_MODEL_PATH = "updated_model.pkl"
--- federated_fraud_system/database/seed/seed_banks.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO banks (bank_name)
-VALUES
-('Bank A'),
-('Bank B'),
-('Bank C');
