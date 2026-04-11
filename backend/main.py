c1de44d57c4df4925736e379d12a1a8d300027e5
Adinath0 <bhawaniadinath@gmail.com>
2026-04-12 02:56:20 +0530

    Delete Check

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,7 @@ from backend.routers import updates
 from backend.routers import audit
 from backend.models import bank_model1
 from backend.models import user_model
-# from backend.models import model_registry
 from backend.models import training_round_model1
-# from backend.models import model_update_model
 from backend.models import audit_log_model   
 
 # create tables
